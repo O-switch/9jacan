602821ee3e331054a2badce79b1c6e0cb8f54a44
Ace <you@example.com>
2024-06-11 15:24:39 +0100

    'initialized'

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll=()=>{
+    menu.classList.remove('active')
+}
+let hamburger = document.querySelector('.hamburger')
+let menu = document.querySelector('menu')
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
